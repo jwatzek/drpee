201b339b1904a7049b190ac818c88e157ea181cd
Julia Watzek <j.watzek@gmail.com>
2021-09-04 20:44:14 -0400

    add command line options

--- motor_test.py
+++ motor_test.py
@@ -1,10 +1,28 @@
+import argparse
 import gpiozero
+import logging
 
-# read motor/relay setup as generic on/off output device
-# start in off position; set so that .on() sets GPIO to LOW
-motor = gpiozero.DigitalOutputDevice(23,
-                                     active_high=False,
-                                     initial_value=False)
+logging.basicConfig(level=logging.INFO)
 
-# make it go :D
-motor.blink(on_time=2, off_time=5, n=3)
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--on', type=float, default=1)
+    parser.add_argument('--off', type=float, default=1)
+    parser.add_argument('--n', type=int, default=2)
+
+    args, _ = parser.parse_known_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    # parse command line arguments
+    args = _parse_args()
+    logging.info(args)
+
+    # read motor/relay setup as generic on/off output device
+    # map .on() to GPIO LOW
+    motor = gpiozero.DigitalOutputDevice(23, active_high=False)
+    motor.blink(on_time=args.on, off_time=args.off, n=args.n, background=False)
